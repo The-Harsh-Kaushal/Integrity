8da1b1131dfc42291bbdebc57a7ad9fce9d711d7
Harsh Kaushal <harshkaushal302@gmail.com>
2025-06-29 20:32:32 +0530

    authentication logic + component

--- frontend/components/Authentication.jsx
+++ frontend/components/Authentication.jsx
@@ -1,26 +1,69 @@
 import React from "react";
 import Inputfield from "./Inputfield";
 import Button from "./Button";
+import { Mail, Eye, EyeOff, CircleUserRound } from "lucide-react";
 
-const Authentication = ({ title = "Login", isLogin = true }) => {
+const Authentication = ({  isLogin = true }) => {
   return (
-    // main div
-    <div className=" flex flex-col p-7 gap-8 border-2 w-[400px] border-amber-400 ">
-      {/* upper section  */}
-      <div className="flex flex-col pl-8">
-        <h1 className=" font-serif text-primary ">{isLogin ? "Welcome Back!" : "Start your journey"}</h1>
-        <p>{isLogin ? "please enter your deatils" : "Sign Up to HIVE"}</p>
+    <div className="flex flex-col w-[400px]  bg-[var(--surface-2)] ">
+      {/* Header Bar */}
+      <div className="pl-3 py-5  bg-gradient-to-r  text-[var(--text)] shine-hover ">
+        <h1 className="text-2xl font-bold font-serif">
+          {isLogin ? "Welcome Back!" : "Create Your Hive Account"}
+        </h1>
+        <p className="text-sm mt-1 opacity-90 pl-6">
+          {isLogin
+            ? "Please enter your credentials to continue"
+            : "Join the swarm and explore"}
+        </p>
       </div>
-      {/* input area  */}
-      <div className="realtive">
-        <Inputfield label="E-mail" isEmail={true} />
-        <Inputfield label="password" isEmail={false} />
+
+      {/* Input Section */}
+      <div className="flex flex-col gap-5 px-6 py-6 shadow-2xl/10 bg-[var(--surface-3)] border-2 border-[var(--border)] rounded-2xl w-[97%]">
+        <Inputfield
+          label="Name"
+          placeholder="full name"
+          switchicon={{
+            switch: false,
+            icon: CircleUserRound,
+          }}
+          types="text"
+        />
+        <Inputfield
+          label="password"
+          placeholder="password"
+          switchicon={{
+            switch: true,
+            icon: {
+              open: Eye,
+              close: EyeOff,
+            },
+          }}
+          types={["password", "text"]}
+        />
+        <Inputfield
+          label="Email"
+          placeholder="example@gmail.com"
+          switchicon={{
+            switch: false,
+            icon: Mail,
+          }}
+          types="text"
+        />
       </div>
-      {/* action area  */}
-      <div>
-        <Button label="Login" />
-        <p>don't have an account</p>
-        <span>Sign In</span>
+
+      {/* Action Section */}
+      <div className="flex flex-col items-center px-6 pb-6 pt-4 gap-3">
+        <Button label={isLogin ? "Sign In" : "Register"} />
+
+        <div className="flex items-center gap-1 text-sm text-[var(--text-muted)] mt-2">
+          <span>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </span>
+          <button className="text-sm text-[var(--primary)] hover:underline transition-all duration-200">
+            {isLogin ? "Sign Up" : "Sign In"}
+          </button>
+        </div>
       </div>
     </div>
   );
